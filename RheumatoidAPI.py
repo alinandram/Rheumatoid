2a42782b509eef97bf710923e0eb9ece75745c75
alinandram <160812576+alinandram@users.noreply.github.com>
2024-02-21 23:52:12 -0500

    Update RheumatoidAPI.py

--- RheumatoidAPI.py
+++ RheumatoidAPI.py
@@ -1,21 +1,9 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-import pickle
-with open('finalized_model.sav','rb') as pickle_file:
-  model = pickle.load(pickle_file)
-
-import pandas as pd
-def convertMapToPandasRow(x):
-    return pd.DataFrame.from_dict(x)
-    
-import numpy as np   
-@app.route('/mymodel', methods = ['POST'])
-def hasRheumatioid():
-    x = request.json
-    pandasRow = convertMapToPandasRow(x)
-    y= model.predict(pandasRow)
-    return np.ndarray.tolist(y)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 app.run()
 
