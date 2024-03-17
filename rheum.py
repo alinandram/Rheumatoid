d4e5b64749219c3c0726ef84cf56720a0413dde6
alinandram <160812576+alinandram@users.noreply.github.com>
2024-03-16 23:29:35 -0400

    Update rheum.py

--- rheum.py
+++ rheum.py
@@ -1,4 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+
+@app.route("/login")
+@cross_origin(supports_credentials=True)
+def login():
+  return jsonify({'success': 'ok'})
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8000, debug=True)
 app = Flask(__name__)
 
 @app.route('/')
