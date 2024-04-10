62e48d1c51bd2ef8f0a5de2a2faad2542c0c9945
mhoss936 <96317037+mhoss936@users.noreply.github.com>
2024-04-10 16:43:48 -0400

    Add files via upload

--- rheum.py
+++ rheum.py
@@ -8,7 +8,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-CORS(app, support_credentials=False)
+CORS(app, support_credentials=False, origins=['https://localhost:3000'])
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
